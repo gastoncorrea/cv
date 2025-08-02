2ae0d81283ca9a70283f85c16278d61906a5893d
unknown <gastoncorrea90e@gmail.com>
2025-08-02 01:13:16 -0300

    Persona model fixed

--- src/main/java/com/cv_personal/backend/model/Persona.java
+++ src/main/java/com/cv_personal/backend/model/Persona.java
@@ -11,33 +11,44 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
 import jakarta.persistence.OneToOne;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-
-@Getter @Setter
+@Getter
+@Setter
 @Entity
 @Table(name = "personas")
 public class Persona {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id_persona;
     @Lob // Indica que el atributo puede almacenar datos grandes (como imágenes)
+    @Size(max = 16_777_215)
     @Column(columnDefinition = "LONGBLOB")
-    private byte[] imagen_perfil;;
+    private byte[] imagen_perfil;
+    @Size(min = 8, max = 500, message = "La descripcion debe tener entre 8 y 500 caracteres")
+    @Column(length = 500, nullable = false)
     private String descripcion_mi;
+    @Size(min = 8, max = 100, message = "La url debe tener entre 8 y 100 caracteres")
+    @Column(length = 100)
     private String url_linkedin;
+    @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres")
+    @Column(length = 50, nullable = false)
     private String nombre;
+    @Size(min = 2, max = 50, message = "El apellido debe tener entre 2 y 50 caracteres")
+    @Column(length = 50, nullable = false)
     private String apellido;
+    @Pattern(regexp = "^[+]?[(]?\\d{3}[)]?[-\\s.]?\\d{3}[-\\s.]?\\d{4,6}$",
+            message = "Número de celular inválido")
     private String num_celular;
-    
+
     @OneToOne
     @JoinColumn(name = "usuario_id") // Clave foránea en la tabla usuario
     private Usuario usuario;
-    
-    
+
 }
