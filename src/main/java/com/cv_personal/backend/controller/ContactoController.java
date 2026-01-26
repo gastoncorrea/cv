0cf1116a25b409d3b95b0bac0a6d92f02858cdc9
unknown <gastoncorrea90e@gmail.com>
2026-01-26 17:07:45 -0300

    Success message in ConctactoController fixed

--- src/main/java/com/cv_personal/backend/controller/ContactoController.java
+++ src/main/java/com/cv_personal/backend/controller/ContactoController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -125,7 +124,7 @@ public class ContactoController {
 
             contacService.deleteContacto(id);
 
-            return ResponseEntity.ok("Educacion eliminada con exito");
+            return ResponseEntity.ok("Contacto eliminado con exito");
 
         }catch(Exception e){
 
